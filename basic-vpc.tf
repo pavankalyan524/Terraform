84521d3fe6fd7870659e486aa038260e2d4ebe66
root <root@ip-172-31-44-80.ap-south-2.compute.internal>
2024-07-29 15:02:38 +0000

    basic vpc file

--- basic-vpc.tf
+++ basic-vpc.tf
@@ -1,9 +1,8 @@
-provider "aws"
-{
-    region = "ap-south-2"
+provider "aws" {
+  region = "ap-south-2"
 }
 
-resource "aws_vpc" {
-    cidr_block = "10.100.0.0/16"
-    instance_tenancy = "Dedicated"
+resource "aws_vpc" "demo" {
+  cidr_block       = "10.100.0.0/16"
+  instance_tenancy = "dedicated"
 }
