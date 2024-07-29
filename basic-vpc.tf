128c4ecfbf2b8c018a330d1463e8e7b9dd729beb
Venkata <venkataganganagunta@gmail.com>
2024-07-29 21:16:49 +0530

    Added Subnet

--- basic-vpc.tf
+++ basic-vpc.tf
@@ -6,3 +6,10 @@ resource "aws_vpc" "demo" {
   cidr_block       = "10.100.0.0/16"
   instance_tenancy = "dedicated"
 }
+
+resource "aws_subnet" "demo"{
+    cidr_block = "10.100.1.0/24"
+    vpc_id = aws_vpc.demo.id
+    availability_zone = "ap-south-2a"
+}
+
