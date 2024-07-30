35721f48335e4cbc31ef0e46521d0e27437c5894
Venkata <venkataganganagunta@gmail.com>
2024-07-30 10:43:41 +0530

    With routes and all

--- basic-vpc.tf
+++ basic-vpc.tf
@@ -76,11 +76,39 @@ resource "aws_instance" "demo"{
 
     }
 
-    ebs_block_device {
+    /*ebs_block_device {
       device_name = "/dev/xvda"
       volume_size = 8
       volume_type = "io1"
       delete_on_termination = true
+    }*/
+
+    resource "aws_route_table" "demo"{
+        vpc_id = aws_vpc.demo.vpc_id
+
+        tags = {
+            name = "Public hyd route table"
+        }
+    }
+
+    resource "aws_route" "demo"{
+        route_table_id = aws_route_table.demo.id
+        destination_cidr_block  = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.demo.id
+    }
+
+    resource "aws_route_table_association" "demo"{
+        subnet_id = aws_subnet.demo.id
+        route_table_id = aws_route_table.demo.id
+
+    }
+
+    resource "aws_internet_gateway" "demo"{
+        vpc_id = aws_vpc.demo.id
+
+        tags = {
+            name ="Demo Internet gateway"
+        }
     }
   
 
