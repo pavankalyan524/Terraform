4a5665bc3fa836f68468fa8f24aaabd1447078f4
Venkata <venkataganganagunta@gmail.com>
2024-08-01 00:17:37 +0530

    modified efs

--- efs/efs.tf
+++ efs/efs.tf
@@ -36,7 +36,7 @@ resource "aws_subnet" "one" {
 
 resource "aws_subnet" "two" {
 
-    idr_block = "10.250.1.0/24"
+    cidr_block = "10.250.1.0/24"
     availability_zone = "ap-south-2b"
     vpc_id = aws_vpc.demo.id
     map_public_ip_on_launch = true
