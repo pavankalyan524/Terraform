5f6304262d16aff9fe8195cb7eb969674184e3ad
pavan <venkatapavan524@gmail.com>
2024-08-03 19:54:54 +0530

    From Vpc to efs with mount

--- efs/efs.tf
+++ efs/efs.tf
@@ -113,7 +113,7 @@ resource "aws_route_table" "demo" {
 
     route {
 
-        destination_cidr_block = "0.0.0.0/0"
+        cidr_block = ["0.0.0.0/0"]
         gateway_id = aws_internet_gateway.igw
 
     }
