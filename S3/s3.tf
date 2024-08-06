83070924701daebad05bb69138b1b9b8841701c8
Venkata <venkataganganagunta@gmail.com>
2024-08-07 00:04:32 +0530

    S3 filee..

--- S3/s3.tf
+++ S3/s3.tf
@@ -19,23 +19,22 @@ provider "aws" {
 resource "aws_s3_bucket" "name" {
 
   bucket = "gvp-venkata-524"
-  acl = "private"
 
 }
 
-/*resource "aws_s3_bucket_acl" "name" {
+resource "aws_s3_bucket_acl" "name" {
 
   acl = "private"
   bucket = aws_s3_bucket.name.id
   
-}*/
+}
 
 resource "aws_s3_bucket_public_access_block" "name" {
 
   bucket = aws_s3_bucket.name.id
 
-  block_public_acls = true
-  ignore_public_acls = true
+  block_public_acls = false
+  ignore_public_acls = false
   block_public_policy = true
   restrict_public_buckets = true
   
